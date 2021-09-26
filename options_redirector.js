4693f02a2ff575bfaee4b7841247c119f4dd318b
JonBurchel <jburchel@microsoft.com>
2021-09-26 14:02:41 -0400

    Updated icons

--- options_redirector.js
+++ options_redirector.js
@@ -1,13 +1,13 @@
 
-chrome.tabs.query({url:[chrome.runtime.getURL('options.html') + "?imagesRendered=true"]},async function(tabs)
+chrome.tabs.query({url:[chrome.runtime.getURL('options.html') + "?imagesRendered=true"]}, function(tabs)
 {
     var popupUrl = chrome.runtime.getURL('options.html');
 
     if(tabs.length > 0)
         chrome.tabs.update(tabs[0].id, { url : popupUrl });
     else
-        await chrome.tabs.create({ url: popupUrl, active: false }, async function(tab) {
-           await chrome.windows.create({ tabId: tab.id, type: 'popup', focused: true, top: 100, left: 100, height: 800, width: 700});
+        chrome.tabs.create({ url: popupUrl, active: false }, function(tab) {
+           chrome.windows.create({ tabId: tab.id, type: 'popup', focused: true, top: 100, left: 100, height: 800, width: 700});
         });
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){ window.close(); })
 });  
