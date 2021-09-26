e0c42e528aff33fc09e88759fe5b43d6c6a7edd0
JonBurchel <jburchel@microsoft.com>
2021-09-26 13:16:38 -0400

    Improve options dialog loading behavior

--- options_redirector.js
+++ options_redirector.js
@@ -1,3 +1,4 @@
+
 chrome.tabs.query({url:[chrome.runtime.getURL('options.html') + "?imagesRendered=true"]},async function(tabs)
 {
     var popupUrl = chrome.runtime.getURL('options.html');
@@ -8,5 +9,5 @@ chrome.tabs.query({url:[chrome.runtime.getURL('options.html') + "?imagesRendered
         await chrome.tabs.create({ url: popupUrl, active: false }, async function(tab) {
            await chrome.windows.create({ tabId: tab.id, type: 'popup', focused: true, top: 100, left: 100, height: 800, width: 700});
         });
-    window.close();
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){ window.close(); })
 });  
