d7a55665b7004e261b4ca6a9299a3518f0ac2687
JonBurchel <jburchel@microsoft.com>
2021-10-10 19:21:37 -0400

    Minor fix

--- background.js
+++ background.js
@@ -18,58 +18,59 @@ chrome.runtime.onInstalled.addListener(()=>{
     contexts: ["selection"],
     id: "ScreenScrubberReplaceSelectionMenu"
   });
+});
 
-  chrome.contextMenus.onClicked.addListener(function(info, tab){
-    if (info.menuItemId == "ScreenScrubberPickImageMenu") {
-      chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ["activatePicker.js"]
-      });
-    }
-    if (info.menuItemId == "ScreenScrubberReplaceSelectionMenu") {
-        chrome.scripting.executeScript({
-          target: {tabId: tab.id},
-          files: ["replaceselection.js"]
-        });
-    } 
-  });
 
-  chrome.action.onClicked.addListener((tab) => {
+chrome.contextMenus.onClicked.addListener(function(info, tab){
+  if (info.menuItemId == "ScreenScrubberPickImageMenu") {
     chrome.scripting.executeScript({
       target: {tabId: tab.id},
-      files: ['content.js']
+      files: ["activatePicker.js"]
     });
-  });
-
-  chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) 
-  {
-    if (msg.from == 'mouseup') 
-    {
-      createScreenshot(function (dataURL) 
-      {           
-        chrome.tabs.query({url:[chrome.runtime.getURL('options.html') + "?imagesRendered=true"]},function(tabs)
-        {
-          var popupUrl = chrome.runtime.getURL('options.html');
-          var qryUrl = popupUrl + 
-            '?top=' + msg.top + 
-            '&left=' + msg.left + 
-            '&height=' + msg.height + 
-            '&width=' + msg.width  +
-            '&imageElement=' + msg.element + 
-            '&imgWidth=' + msg.imgWidth +
-            '&pixelRatio=' + msg.pixelRatio +
-            '&screenImg=' + dataURL;
-          if(tabs.length > 0)
-          {
-              chrome.tabs.update(tabs[0].id, { url : qryUrl });
-          }
-          else
-            chrome.tabs.create({ url: qryUrl, active: false }, function(tab) {
-              chrome.windows.create({ tabId: tab.id, type: 'popup', focused: true, top: 100, left: 100, height: 775, width: 700});
-            });
-        });           
+  }
+  if (info.menuItemId == "ScreenScrubberReplaceSelectionMenu") {
+      chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ["replaceselection.js"]
       });
-    }
+  } 
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ['content.js']
   });
-})
+});
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) 
+{
+  if (msg.from == 'mouseup') 
+  {
+    createScreenshot(function (dataURL) 
+    {           
+      chrome.tabs.query({url:[chrome.runtime.getURL('options.html') + "?imagesRendered=true"]},function(tabs)
+      {
+        var popupUrl = chrome.runtime.getURL('options.html');
+        var qryUrl = popupUrl + 
+          '?top=' + msg.top + 
+          '&left=' + msg.left + 
+          '&height=' + msg.height + 
+          '&width=' + msg.width  +
+          '&imageElement=' + msg.element + 
+          '&imgWidth=' + msg.imgWidth +
+          '&pixelRatio=' + msg.pixelRatio +
+          '&screenImg=' + dataURL;
+        if(tabs.length > 0)
+        {
+            chrome.tabs.update(tabs[0].id, { url : qryUrl });
+        }
+        else
+          chrome.tabs.create({ url: qryUrl, active: false }, function(tab) {
+            chrome.windows.create({ tabId: tab.id, type: 'popup', focused: true, top: 100, left: 100, height: 775, width: 700});
+          });
+      });           
+    });
+  }
+});
 
