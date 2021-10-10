0277fd206fad4ef6aae5719bdde24d8ebe7318c8
JonBurchel <jburchel@microsoft.com>
2021-10-10 19:12:51 -0400

    Adds basic selection replacement on right-click - still needs fancy dialog

--- background.js
+++ background.js
@@ -2,57 +2,74 @@ function createScreenshot(callback) {
   chrome.tabs.captureVisibleTab(null, { format: "png" }, callback);
 }
 
+function ReplaceSelectedText() {
+  alert(0);
+}
+
 chrome.runtime.onInstalled.addListener(()=>{
   chrome.contextMenus.create({
     title:"Pick an image on the current page to replace",
     contexts:["action"],
     id: "ScreenScrubberPickImageMenu",
   });
-})
 
-chrome.action.onClicked.addListener((tab) => {
+  chrome.contextMenus.create({
+    title:"Replace selected text with Screenshot Scrubber",
+    contexts: ["selection"],
+    id: "ScreenScrubberReplaceSelectionMenu"
+  });
+
+  chrome.contextMenus.onClicked.addListener(function(info, tab){
+    if (info.menuItemId == "ScreenScrubberPickImageMenu") {
+      chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ["activatePicker.js"]
+      });
+    }
+    if (info.menuItemId == "ScreenScrubberReplaceSelectionMenu") {
+        chrome.scripting.executeScript({
+          target: {tabId: tab.id},
+          files: ["replaceselection.js"]
+        });
+    } 
+  });
+
+  chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: {tabId: tab.id},
       files: ['content.js']
     });
   });
 
-chrome.contextMenus.onClicked.addListener(function(info, tab){
-  if (info.menuItemId == "ScreenScrubberPickImageMenu") {
-      chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ["activatePicker.js"]
-      });
-    } 
-  });
-
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) 
-{
-  if (msg.from == 'mouseup') 
+  chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) 
   {
-    createScreenshot(function (dataURL) 
-    {           
-      chrome.tabs.query({url:[chrome.runtime.getURL('options.html') + "?imagesRendered=true"]},function(tabs)
-      {
-        var popupUrl = chrome.runtime.getURL('options.html');
-        var qryUrl = popupUrl + 
-          '?top=' + msg.top + 
-          '&left=' + msg.left + 
-          '&height=' + msg.height + 
-          '&width=' + msg.width  +
-          '&imageElement=' + msg.element + 
-          '&imgWidth=' + msg.imgWidth +
-          '&pixelRatio=' + msg.pixelRatio +
-          '&screenImg=' + dataURL;
-        if(tabs.length > 0)
+    if (msg.from == 'mouseup') 
+    {
+      createScreenshot(function (dataURL) 
+      {           
+        chrome.tabs.query({url:[chrome.runtime.getURL('options.html') + "?imagesRendered=true"]},function(tabs)
         {
-            chrome.tabs.update(tabs[0].id, { url : qryUrl });
-        }
-        else
-          chrome.tabs.create({ url: qryUrl, active: false }, function(tab) {
-            chrome.windows.create({ tabId: tab.id, type: 'popup', focused: true, top: 100, left: 100, height: 775, width: 700});
-          });
-      });           
-    });
-  }
-});
+          var popupUrl = chrome.runtime.getURL('options.html');
+          var qryUrl = popupUrl + 
+            '?top=' + msg.top + 
+            '&left=' + msg.left + 
+            '&height=' + msg.height + 
+            '&width=' + msg.width  +
+            '&imageElement=' + msg.element + 
+            '&imgWidth=' + msg.imgWidth +
+            '&pixelRatio=' + msg.pixelRatio +
+            '&screenImg=' + dataURL;
+          if(tabs.length > 0)
+          {
+              chrome.tabs.update(tabs[0].id, { url : qryUrl });
+          }
+          else
+            chrome.tabs.create({ url: qryUrl, active: false }, function(tab) {
+              chrome.windows.create({ tabId: tab.id, type: 'popup', focused: true, top: 100, left: 100, height: 775, width: 700});
+            });
+        });           
+      });
+    }
+  });
+})
+
---
+++ replaceselection.js
@@ -0,0 +1,78 @@
+function getSelectionCoords(win) {
+    win = win || window;
+    var doc = win.document;
+    var sel = doc.selection, range, rects, rect;
+    var x = 0, y = 0;
+    if (sel) {
+        if (sel.type != "Control") {
+            range = sel.createRange();
+            range.collapse(true);
+            x = range.boundingLeft;
+            y = range.boundingTop;
+        }
+    } else if (win.getSelection) {
+        sel = win.getSelection();
+        if (sel.rangeCount) {
+            range = sel.getRangeAt(0).cloneRange();
+            if (range.getClientRects) {
+                range.collapse(true);
+                rects = range.getClientRects();
+                if (rects.length > 0) {
+                    rect = rects[0];
+                }
+                x = rect.left;
+                y = rect.top;
+            }
+            // Fall back to inserting a temporary element
+            if (x == 0 && y == 0) {
+                var span = doc.createElement("span");
+                if (span.getClientRects) {
+                    // Ensure span has dimensions and position by
+                    // adding a zero-width space character
+                    span.appendChild( doc.createTextNode("\u200b") );
+                    range.insertNode(span);
+                    rect = span.getClientRects()[0];
+                    x = rect.left;
+                    y = rect.top;
+                    var spanParent = span.parentNode;
+                    spanParent.removeChild(span);
+
+                    // Glue any broken text nodes back together
+                    spanParent.normalize();
+                }
+            }
+        }
+    }
+    return { x: x, y: y };
+}
+
+function escapeRegex(string) {
+    return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+
+function findAndReplace(searchText, replacement, searchNode) {
+    var regex = new RegExp("((?<=>)[^<]*)(" + escapeRegex(searchText) + ")([^<]*)", 'g');
+    try { 
+        searchNode.innerHTML = searchNode.innerHTML.replace(regex, "$1" + replacement + "$3") 
+    } catch(e) {} // ignore any errors caused by script replacement on the page
+}
+
+var mousePos = getSelectionCoords(window);
+var elems = document.elementsFromPoint(mousePos.x, mousePos.y);
+
+var replaceText = prompt("Replacement text:");
+var replaceAll = confirm("Click OK to replace ALL occurrences or Cancel to replace just this one.");
+
+if (!replaceAll)
+    for (var i = 0; i < elems.length; i++)
+    {
+        if (elems[i].innerHTML.search(document.getSelection().toString()) != 0);
+        {
+            elems[i].innerHTML = elems[i].innerHTML.replace(document.getSelection().toString(), replaceText);
+            i = elems.length;
+        }
+    }
+else
+    findAndReplace(document.getSelection().toString(), replaceText, document.body);
+
+
