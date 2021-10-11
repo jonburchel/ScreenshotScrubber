f419e2d5f47d6bd191fcb55dc0a2d5d510891beb
JonBurchel <jburchel@microsoft.com>
2021-10-11 02:33:28 -0400

    Some layout improvement for replace text dialog

--- replaceselection.js
+++ replaceselection.js
@@ -66,12 +66,13 @@ var divDialog = document.body.insertBefore(document.createElement('div'), docume
 divDialog.style="position:absolute;z-index:2147483647;left:0;top:0;width:100%;height:100%;";
 divDialog.className = "ScreenScrubberReplacePromptOverlay";
 divDialog.id = divDialog.className;
-divDialog.innerHTML = "<div style='border: solid gray 2px; position:fixed; top: " + ((window.innerHeight / 2) - 85) + "px; left: " + ((window.innerWidth / 2) - 200) + "px; width: 400px; height: 170px; background-color: cornflowerblue;'>\
+divDialog.innerHTML = "<div style='border: solid gray 2px; position:fixed; top: " + ((window.innerHeight / 2) - 85) + "px; left: " + ((window.innerWidth / 2) - 200) + "px;width: \
+                                400px; height: 170px; background-color: cornflowerblue;font-family: Segoe UI;font-weight: 700;'>\
     <table width=100% height=100%>\
-        <tr style='border-bottom: solid gray 2px; height: 30px; font-size: large;'><td width=100% colspan=2>\
-            &nbsp;<img width=24 style='position:relative; top:4px;' src='" + chrome.runtime.getURL("images/DocScreenshotScrubberIcon32.png") + "'/><b style='position:relative; top:-4px; left: 8px;'>Screenshot Scrubber - Replace Text</b></td></tr>\
-        <tr style='height: 20px;'><td>&nbsp;&nbsp;<b>Search for:</b></td><td><input type=text style='padding: 0px; font-size:small;' size=45 id='ScreenshotScrubberSearchFor' value='" +  document.getSelection().toString() + "'/></td></tr>\
-        <tr style='height: 20px;'><td>&nbsp;&nbsp;<b>Replace with:</b></td><td><input type=text style='padding: 0px; font-size:small;'' size=45 id='ScreenshotScrubberReplace'/></td></tr>\
+        <tr style='border-bottom: solid gray 2px; height: 30px; font-size: large;'><td width=100% colspan=2 style='text-align:left;'>\
+            &nbsp;<img width=24 style='float:left;position:relative; top:4px;left: 3px;' src='" + chrome.runtime.getURL("images/DocScreenshotScrubberIcon32.png") + "'/><b style='position:relative; top:2px; left: 8px;'>Screenshot Scrubber - Replace Text</b></td></tr>\
+        <tr style='height: 20px;font-size:small;'><td>&nbsp;&nbsp;<b>Search for:</b></td><td><input type=text style='font-family: Segoe UI;padding: 0px; font-size:small;' size=45 id='ScreenshotScrubberSearchFor' value='" +  document.getSelection().toString() + "'/></td></tr>\
+        <tr style='height: 20px;font-size:small;'><td>&nbsp;&nbsp;<b>Replace with:</b></td><td><input type=text style='font-family: Segoe UI;padding: 0px; font-size:small;'' size=45 id='ScreenshotScrubberReplace'/></td></tr>\
         <tr><td colspan=2 style='text-align:right;'>" + 
             (foundCount <= 1 ? "" : "<b>Replace all " + foundCount + " occurrences:&nbsp;</b><input type=checkbox checked id='ScreenshotScrubberReplaceAll'/>&nbsp;&nbsp;<br>") +
             "<b>Save to default configuration:&nbsp;</b><input type=checkbox checked id='ScreenshotScrubberSaveToConfig' />&nbsp;&nbsp;\
@@ -92,6 +93,7 @@ document.getElementById("ScreenshotScrubberReplaceButton").addEventListener("cli
     document.getElementById("ScreenScrubberReplacePromptOverlay").remove();
     if(updateConfig) {
         chrome.storage.sync.get("ConfigArray", function(ca) {
+            console.log(ca);
             if (ca.ConfigArray == null)
             {
                 var DefaultSettings = new Array(5);
@@ -100,6 +102,7 @@ document.getElementById("ScreenshotScrubberReplaceButton").addEventListener("cli
                 DefaultSettings[2] = ["<your name>", "Chris Q. Public"];
                 DefaultSettings[3] = ["<youralias@microsoft.com>", "chrisqpublic@contoso.com"];
                 DefaultSettings[4] = [searchText, replaceText];
+                chrome.storage.sync.set({ConfigArray: ca.ConfigArray});
             }
             else
             {
