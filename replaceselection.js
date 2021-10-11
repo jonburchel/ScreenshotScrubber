5fd86023809866b0996adf45949ab7162e91277c
JonBurchel <jburchel@microsoft.com>
2021-10-11 02:52:20 -0400

    Style improvement on replacement dialog

--- replaceselection.js
+++ replaceselection.js
@@ -71,9 +71,9 @@ divDialog.innerHTML = "<div style='border: solid gray 2px; position:fixed; top: 
     <table width=100% height=100%>\
         <tr style='border-bottom: solid gray 2px; height: 30px; font-size: large;'><td width=100% colspan=2 style='text-align:left;'>\
             &nbsp;<img width=24 style='float:left;position:relative; top:4px;left: 3px;' src='" + chrome.runtime.getURL("images/DocScreenshotScrubberIcon32.png") + "'/><b style='position:relative; top:2px; left: 8px;'>Screenshot Scrubber - Replace Text</b></td></tr>\
-        <tr style='height: 20px;font-size:small;'><td>&nbsp;&nbsp;<b>Search for:</b></td><td><input type=text style='font-family: Segoe UI;padding: 0px; font-size:small;' size=45 id='ScreenshotScrubberSearchFor' value='" +  document.getSelection().toString() + "'/></td></tr>\
-        <tr style='height: 20px;font-size:small;'><td>&nbsp;&nbsp;<b>Replace with:</b></td><td><input type=text style='font-family: Segoe UI;padding: 0px; font-size:small;'' size=45 id='ScreenshotScrubberReplace'/></td></tr>\
-        <tr><td colspan=2 style='text-align:right;'>" + 
+        <tr style='height: 20px;font-size:small;'><td>&nbsp;&nbsp;<b>Search for:</b></td><td><input type=text style='margin-bottom: 5px; margin-top: 5px;border-width: 1px;font-family: Segoe UI;padding: 0px; font-size:small;' size=45 id='ScreenshotScrubberSearchFor' value='" +  document.getSelection().toString() + "'/></td></tr>\
+        <tr style='height: 20px;font-size:small;'><td>&nbsp;&nbsp;<b>Replace with:</b></td><td><input type=text style='margin-bottom: 5px; margin-top: 5px;border-width: 1px;font-family: Segoe UI;padding: 0px; font-size:small;'' size=45 id='ScreenshotScrubberReplace'/></td></tr>\
+        <tr><td colspan=2 style='text-align:right;font-size:small;'>" + 
             (foundCount <= 1 ? "" : "<b>Replace all " + foundCount + " occurrences:&nbsp;</b><input type=checkbox checked id='ScreenshotScrubberReplaceAll'/>&nbsp;&nbsp;<br>") +
             "<b>Save to default configuration:&nbsp;</b><input type=checkbox checked id='ScreenshotScrubberSaveToConfig' />&nbsp;&nbsp;\
         </td></tr>\
